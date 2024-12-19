2d2cd79a251765bb7a81007ab9251fd509ce95e6
shamim888 <shamimnw99@gmail.com>
2024-12-19 18:39:19 +0600

    color palettes added to tailwind.config

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,7 +1,23 @@
 module.exports = {
   content: ["./public/**/*.html"],
   theme: {
-    extend: {},
+    extend: {
+      colors: {
+        primaryColor : '#52BBDC',
+        secondaryColor : '#2B2B2B',
+        thirdColor : '#EBECF0',
+        fourthColor : '#9A9797',
+        fifthColor : '#13AEDF',
+        sixthColor : '#78B7A8',
+        seventhColor : '#C4C4C4',
+        eighthColor : '#7B7979',
+        ninethColor : '#FFF7F7',
+        tenthColor : '#464646',
+        eleventhColor : '#A0C9D1',
+        twelvethColor : '#5FE0B2',
+        thirteenthColor : '#FFEFED',
+      },
+    },
   },
   plugins: [],
 }
